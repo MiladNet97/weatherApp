3c3fc34b85022905f14b379fed8cc4ec14a9e5c9
Milad Shahsavari <miladshahsavari1@gmial.com>
2021-09-04 13:12:08 +0430

    Update all files

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-// show date & time in html
-const showDate = () => {
-  let nowDate = new Date(),
-    month = nowDate.toLocaleString('default', {
-      month: 'short'
-    }),
-    date = nowDate.getDate(),
-    hours = ('0' + nowDate.getHours()).slice(-2),
-    minutes = ('0' + nowDate.getMinutes()).slice(-2),
-    seconds = ('0' + nowDate.getSeconds()).slice(-2);
-
-  // Access the .show__date for the display date and time 
-  const showDateTime = document.querySelector('.show__date');
-  showDateTime.innerHTML = `${month} ${date}, ${hours}:${minutes}:${seconds}`
+// Get api Time
+const getTime = async (timeZone) => {
+  const url = `https://api.ipgeolocation.io/timezone?apiKey=cfefae5f618a4ef58da0140b5a6429d0&tz=${timeZone}`;
+  const response = await fetch(url);
+  const data = await response.json()
+  return data
 }
-
-// Refresh the showDate function every 1 second 
-setInterval(() => showDate(), 1000);
-
 
 // Get api location
 const getLocation = async () => {
-  const url = 'http://ip-api.com/json/?fields=status,country,city,lat,lon';
+  const url = 'http://ip-api.com/json/?fields=country,city,lat,lon,timezone';
   const response = await fetch(url);
   const data = await response.json()
 
@@ -87,3 +75,70 @@ const getTemp = weTemp => {
     can: Math.floor(c)
   };
 }
+
+// Vriables
+let tempData = document.querySelector('.temperature__data'),
+  tempUnit = document.querySelector('.temperature__unit'),
+  tempCel = document.querySelector('.temperature__cel'),
+  tempFar = document.querySelector('.temperature__far'),
+  tempDes = document.querySelector('.temperature__description'),
+  country = document.querySelector('.weather__country'),
+  city = document.querySelector('.weather__city'),
+  humidity = document.querySelector('.weather__humidity'),
+  windSpeed = document.querySelector('.weather__speed'),
+  weIcon = document.querySelector('.weather__icon');
+
+
+window.addEventListener('load', () => {
+  getLocation()
+    .then(locData => {
+      country.textContent = locData.country;
+      city.textContent = locData.city;
+      getWeather(locData.lat, locData.lon)
+        .then(weData => {
+          const weTemp = weData.main.temp;
+          weMain = weData.weather[0].main,
+            weDes = weData.weather[0].description,
+            weHumidity = weData.main.humidity,
+            wiSpeed = weData.wind.speed;
+
+          // Show temperature in html 
+          tempData.textContent = getTemp(weTemp).can;
+          tempFar.classList.add('disable')
+
+          tempUnit.addEventListener('click', e => {
+            if (e.target.classList.contains('temperature__cel')) {
+              tempFar.classList.add('disable')
+              tempCel.classList.remove('disable')
+              tempData.textContent = getTemp(weTemp).can;
+            } else if (e.target.classList.contains('temperature__far')) {
+              tempCel.classList.add('disable')
+              tempFar.classList.remove('disable')
+              tempData.textContent = getTemp(weTemp).far;
+            }
+          })
+
+          // Show temperature description in html 
+          tempDes.textContent = weDes;
+
+          // Show temperature icon in html 
+          weIcon.src = `icons/${getIcon(weMain)}`
+
+          // Show humidity in html 
+          humidity.textContent = weHumidity + '%';
+
+          // Show wind speed in html 
+          windSpeed.textContent = Math.floor(wiSpeed * 3.6) + ' km/h';
+        })
+
+      getTime(locData.timezone)
+        .then(timeData => {
+          let data = timeData.date_time_wti.slice(5, 11),
+            time = timeData.time_24.slice(0, 5);
+
+          // Access the .show__date for the display date and time 
+          const showDateTime = document.querySelector('.show__date');
+          showDateTime.innerHTML = `${data}, ${time}`
+        })
+    })
+})
